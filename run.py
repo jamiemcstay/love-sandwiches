a60aea68275e29e60090c8003801382686b955fe
Jamie Mcstay <jamiemcstay@hotmail.com>
2024-07-16 15:04:10 +0000

    Add sales data to sales worksheet

--- run.py
+++ run.py
@@ -24,8 +24,6 @@ def get_sales_data():
         data_str = input("Enter your data here: ")
         sales_data = data_str.split(",")
 
-        validate_data(sales_data)
-
         if validate_data(sales_data):
             print("Data is valid!")
             break
@@ -51,7 +49,19 @@ def validate_data(values):
     
     return True
 
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add nnew row with the list data provided.
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
+
 data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
+
 
 
 
